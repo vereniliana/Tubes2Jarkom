dac10950cd84e15d5c47a28f2cf62500b0546222
vereniliana <vereniliana@gmail.com>
2017-11-29 16:07:09 +0700

    *

--- jarkom.cpp
+++ jarkom.cpp
@@ -7,7 +7,7 @@ typedef struct {
 	int nextHop;
 } edge;
 
-edge matriks[1010][1010];
+edge matriks[1010][1010]; // matriks global berisi pair distance dan nextHop
 
 void print (int n) {
 	for (int i=1; i<=n; i++) {
@@ -19,11 +19,19 @@ void print (int n) {
 }
 
 int main() {
-	int n, m, i, u, v;
+	int n, m, i, u, v, s, x, y; 
+	// n : jumlah node(N)
+	// m : jumlah edge(M)
+	// inisialisasi node u terhubung ke node v 
+	// s : jumlah skenario
+	// node x mengirim ke node y di update
 	
+	/* inisialisasi matriks */
+	/* i: node, j: routing table dari node i */
+	/* matriks[1][1] s.d. matriks[1][5] berarti smuanya routing table node 1 */
 	for (int i=1; i<=1000; i++) {
 		for (int j=1; j<=1000; j++) {
-			if (i == j) {
+			if (i == j) { // hubungan dengan diri sendiri
 				matriks[i][j].distance = 0;
 				matriks[i][j].nextHop = i;
 			} else {
@@ -35,6 +43,7 @@ int main() {
 	
 	scanf("%d %d", &n, &m);
 	
+	/* inisialisasi matriks menurut input */
 	for (i=1; i<=m; i++) {
 		scanf("%d %d", &u, &v);
 		matriks[u][v].distance =  1;
@@ -43,6 +52,13 @@ int main() {
 		matriks[v][u].nextHop = u;
 	}
 	
+	scanf("%d", &s);
+	/*
+	for (i=1; i<=s; i++) {
+		scanf("%d %d", &x, &y);
+		
+	}
+	*/
 	print(n);
 	return 0;
 }
