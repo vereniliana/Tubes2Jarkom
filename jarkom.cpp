29389b089f41a4c3b6e3f349b484cfae59436601
Audry Nyonata <audrynyonata@gmail.com>
2017-11-29 16:35:20 +0700

    final

--- jarkom.cpp
+++ jarkom.cpp
@@ -7,19 +7,10 @@ typedef struct {
 	int nextHop;
 } edge;
 
-edge matriks[1010][1010]; // matriks global berisi pair distance dan nextHop
-
-void print (int n) {
-	for (int i=1; i<=n; i++) {
-		for (int j=1; j<=n; j++) {
-			printf("%d:%d ", matriks[i][j].distance, matriks[i][j].nextHop);
-		}
-		printf("\n");
-	}
-}
 
 int main() {
 	int n, m, i, u, v, s, x, y; 
+	edge matriks[1010][1010]; // matriks global berisi pair distance dan nextHop
 	// n : jumlah node(N)
 	// m : jumlah edge(M)
 	// inisialisasi node u terhubung ke node v 
@@ -35,7 +26,7 @@ int main() {
 				matriks[i][j].distance = 0;
 				matriks[i][j].nextHop = i;
 			} else {
-				matriks[i][j].distance = INT_MAX;
+				matriks[i][j].distance = INT_MAX-1;
 				matriks[i][j].nextHop = -1;
 			}
 		}
@@ -53,12 +44,27 @@ int main() {
 	}
 	
 	scanf("%d", &s);
-	/*
+	
 	for (i=1; i<=s; i++) {
 		scanf("%d %d", &x, &y);
-		
+		for(int j=1;j<=n;j++) {
+			if (matriks[x][j].distance+1 < matriks[y][j].distance || 
+				(matriks[x][j].distance+1 == matriks[y][j].distance && x < matriks[y][j].nextHop)) {
+					matriks[y][j].distance = matriks[x][j].distance+1;
+					matriks[y][j].nextHop = x;
+					
+			}
+		}
 	}
-	*/
-	print(n);
+	
+	for (int i=1; i<=n; i++) {
+		for (int j=1; j<=n; j++) {
+			if (matriks[i][j].distance == INT_MAX-1) {
+				matriks[i][j].distance= -1;
+			}
+			printf("%d %d\n", matriks[i][j].distance, matriks[i][j].nextHop);
+		}
+	}
+	
 	return 0;
 }
